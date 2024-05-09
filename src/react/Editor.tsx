d8c1f4ddd5c9fd6f2e662ab54d5954f20bd92d7f
David Farthing <davidfarthing@deltek.com>
2024-05-09 15:52:11 -0400

    Render plain HTML instead of CKEditor when editor content isn't focused

--- src/react/Editor.tsx
+++ src/react/Editor.tsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import parse from 'html-react-parser';
+
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 
 import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
@@ -19,66 +21,71 @@ export interface EditorProps {
 
 const Editor: React.FC<EditorProps> = (
     {
-        initialData,
+        initialData = "<p>Hello world!</p>",
         channelId,
         editorSuffix = "1",
     }) => {
 
+    const [isActive, setIsActive] = React.useState(false);
     const editorId = 'TestEditor' + editorSuffix;
+    const [editorData, setEditorData] = React.useState(initialData);
     return (
         <div>
-            <CKEditor
-                editor={ClassicalEditorBuild.ClassicEditor}
-                config={{
-                    collaboration: {
-                        channelId: channelId + editorSuffix,
-                    },
-                    bracketOption: {
-                        bracketOptionRenderer: (
-                            id: string,
-                            value: string,
-                            optedState: OptedState,
-                            onOptedStateChanged: (newState: OptedState) => void,
-                            domElement: HTMLElement,
-                        ) => {
-                            const root = createRoot(domElement);
+            {isActive ?
+                <CKEditor
+                    editor={ClassicalEditorBuild.ClassicEditor}
+                    config={{
+                        collaboration: {
+                            channelId: channelId + editorSuffix,
+                        },
+                        bracketOption: {
+                            bracketOptionRenderer: (
+                                id: string,
+                                value: string,
+                                optedState: OptedState,
+                                onOptedStateChanged: (newState: OptedState) => void,
+                                domElement: HTMLElement,
+                            ) => {
+                                const root = createRoot(domElement);
 
-                            root.render(
-                                <BracketOption id={id} value={value} initialOptedState={optedState} onOptedStateChanged={onOptedStateChanged} />
-                            );
-                        }
-                    },
-                }}
-                data={initialData}
-                onReady={editor => {
-                    // Attach inspector
-                    CKEditorInspector.detach(editorId + editorSuffix);
-                    CKEditorInspector.attach({ editorId: editor });
+                                root.render(
+                                    <BracketOption id={id} value={value} initialOptedState={optedState} onOptedStateChanged={onOptedStateChanged} />
+                                );
+                            }
+                        },
+                    }}
+                    data={editorData}
+                    onReady={editor => {
+                        // Attach inspector
+                        CKEditorInspector.detach(editorId + editorSuffix);
+                        CKEditorInspector.attach({ editorId: editor });
 
-                    // Hide toolbar by default and show it when editor is focused
-                    const toolbarElement = editor.ui.view.toolbar.element;
-                    if (toolbarElement) {
-                        toolbarElement.style.display = 'none';
-                    }
-                    editor.ui.focusTracker.on( 'change:isFocused', ( evt, data, isFocused ) => {
-                        const toolbar = editor.ui.view.toolbar.element;
-                        if (toolbar) {
-                            toolbar.style.display = isFocused ? 'flex' : 'none';
-                        }
-                    } );
+                        // Render plain HTML rather than CKEditor when the editor loses focus.
+                        // This is to test on-demand "activation" of CKEditor so we don't have to connect
+                        // all of them to Real-Time-Collaboration at once.
+                        // (CKEditor5 doesn't yet have a simply way to connect/disconnect from RTC.)
+                        editor.ui.focusTracker.on('change:isFocused', (evt, data, isFocused) => {
+                            if (!isFocused) {
+                                setEditorData(editor.getData());
+                                setIsActive(false);
+                            }
+                        });
 
-                    console.log(`Editor "${editorId}" is ready to use!`, editor);
-                }}
-                onChange={(event) => {
-                    console.log(event);
-                }}
-                onBlur={(event, editor) => {
-                    console.log('Blur.', editor);
-                }}
-                onFocus={(event, editor) => {
-                    console.log('Focus.', editor);
-                }}
-            />
+                        console.log(`Editor "${editorId}" is ready to use!`, editor);
+                    }}
+                    onChange={(event) => {
+                        console.log(event);
+                    }}
+                    onBlur={(event, editor) => {
+                        console.log('Blur.', editor);
+                    }}
+                    onFocus={(event, editor) => {
+                        console.log('Focus.', editor);
+                    }}
+                /> :
+                <div onClick={() => setIsActive(true)}>
+                    {parse(editorData!)}
+                </div>}
         </div>
     );
 }
